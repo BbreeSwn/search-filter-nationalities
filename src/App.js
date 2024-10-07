61cd42861756cbd220cd12cd63105fd1e49186d3
BbreeSwn <suwanan.cs@hotmail.com>
2024-10-07 14:36:07 +0700

    feat:set data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+const data = [
+  {id:1,name:"Thailand",region:"Asia",population:100},
+  {id:2,name:"Japan",region:"Asia",population:50},
+]
+
+const [countries , setCountries] = useState(data)
+
   return (
-    <div className="App">
-    <h1>Helllo</h1>
+    <div className='container'>
+<ul>
+{countries.map((item)=>{
+return<li key={item.id}>{item.name} | {item.region}</li>
+    })}
+</ul>
     </div>
   );
 }
